e438d33ed23e80b20793a098ddc0f360363c57f8
Alfonso Vargas <wk.alfonso@gmail.com>
2018-08-21 23:09:33 -0500

    Creación de scroll infinito

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final wordPair = new WordPair.random();
+
     return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+        title: 'Wecome to Fluter',
+        home: new RandomWords()
     );
   }
 }
@@ -106,4 +97,52 @@ class _MyHomePageState extends State<MyHomePage> {
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+class RandomWordsState extends State<RandomWords> {
+  final List<WordPair> _suggestions = <WordPair>[];
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
+
+
+  @override
+  Widget build(BuildContext context) {
+    /*final wordPair = new WordPair.random();
+    return new Text(wordPair.asPascalCase);*/
+
+    return new Scaffold(
+      appBar: new AppBar(title: new Text("Startup Name Generator")),
+        body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return new ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (BuildContext context, int i) {
+        if(i.isOdd)
+          return new Divider();
+
+        final int index = i ~/ 2;
+        if(index >= _suggestions.length)
+          _suggestions.addAll(generateWordPairs().take(10));
+
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return new ListTile(
+      title: new Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+
+  @override
+  RandomWordsState createState() => new RandomWordsState();
 }
