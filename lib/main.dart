5915b74c0c30eaf312c4c852e4f19cb53e6d6b25
Alfonso Vargas <wk.alfonso@gmail.com>
2018-08-21 23:29:27 -0500

    Icono de favorito + navegación

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,9 @@ class MyApp extends StatelessWidget {
 
     return new MaterialApp(
         title: 'Wecome to Fluter',
+        theme: new ThemeData(
+          primaryColor: Colors.white
+        ),
         home: new RandomWords()
     );
   }
@@ -101,8 +104,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
 class RandomWordsState extends State<RandomWords> {
   final List<WordPair> _suggestions = <WordPair>[];
+  final Set<WordPair> _saved = new Set<WordPair>();
   final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
-
 
   @override
   Widget build(BuildContext context) {
@@ -110,8 +113,13 @@ class RandomWordsState extends State<RandomWords> {
     return new Text(wordPair.asPascalCase);*/
 
     return new Scaffold(
-      appBar: new AppBar(title: new Text("Startup Name Generator")),
-        body: _buildSuggestions(),
+      appBar: new AppBar(
+          title: new Text("Startup Name Generator"),
+        actions: <Widget>[
+          new IconButton(icon: const Icon(Icons.list), onPressed: _pushSaved)
+        ],
+      ),
+      body: _buildSuggestions(),
     );
   }
 
@@ -132,12 +140,54 @@ class RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
     return new ListTile(
       title: new Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+      trailing: new Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if(alreadySaved)
+            _saved.remove(pair);
+          else
+            _saved.add(pair);
+        });
+      },
     );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(new MaterialPageRoute(
+        builder: (BuildContext context)  {
+          final Iterable<ListTile> tiles = _saved.map(
+              (WordPair pair) {
+                return new ListTile (
+                  title: new Text(
+                    pair.asPascalCase,
+                    style: _biggerFont,
+                  )
+                );
+              }
+          );
+
+          final List<Widget> divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles
+          ).toList();
+
+          return new Scaffold(
+            appBar: new AppBar(
+              title: const Text("Saved Suggestions"),
+            ),
+            body: new ListView(children: divided),
+          );
+        }
+    ));
   }
 }
 
